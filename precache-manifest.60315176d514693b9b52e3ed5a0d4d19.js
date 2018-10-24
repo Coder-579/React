519036a5b96f4f41d1417b826391ba9668754885
Coder-579 <susers6edge@gmail.com>
2018-10-24 15:54:20 +0600

    Updates

---
+++ precache-manifest.60315176d514693b9b52e3ed5a0d4d19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdeb66dadab5814ea446",
+    "url": "/React/static/js/runtime~main.bdeb66da.js"
+  },
+  {
+    "revision": "9deea69b10d1c31c3ccc",
+    "url": "/React/static/js/main.9deea69b.chunk.js"
+  },
+  {
+    "revision": "87313a20f1905e6b55b6",
+    "url": "/React/static/js/1.87313a20.chunk.js"
+  },
+  {
+    "revision": "9deea69b10d1c31c3ccc",
+    "url": "/React/static/css/main.9b5f7592.chunk.css"
+  },
+  {
+    "revision": "7fddd583d0d21d0c6dd0c515eba3fa3e",
+    "url": "/React/index.html"
+  }
+];
